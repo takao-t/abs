4179a39337fb1c0c48a15fa5a5fd89c4621f705d
Takao Takahashi <tsq@gapj.net>
2020-11-05 10:38:14 +0900

    change door phone peer name

--- exten/phonegen.sh
+++ exten/phonegen.sh
@@ -41,7 +41,7 @@ echo ";ビデオドアホン用(H.264 GS向け)"
 for i in 1 2
 do
     echo "[doorphone$i](phone)"
-    echo "username=phone$i"
+    echo "username=door$i"
     SALT=`date +%N`
     SECBASE="phone:$SALT:$i"
     SECRET=`echo $SECBASE | md5sum | cut -f1,1 -d' '`
