ceda1446937b486c6e699bae9f730dce3f18fb37
TakaoTakahashi <tsq@gapj.net>
2023-01-31 11:20:59 +0900

    add some mac

--- panel/php/addon/mac_vendor.php
+++ panel/php/addon/mac_vendor.php
@@ -2,7 +2,9 @@
 
     //端末のピア情報識別に使用
     //arrayでベンダーコード部を列挙する
-    $mac_panasonic_vc = array('00:80:F0','BC:C3:42');
-    $mac_gs_vc = array('00:0B:82','AC:A8:8E:57:F3:71');
+    //$mac_panasonic_vc = array('00:80:F0','BC:C3:42');
+    $mac_panasonic_vc = array('00:80:F0','BC:C3:42','00:11:22');
+    //$mac_gs_vc = array('00:0B:82','AC:A8:8E:57:F3:71');
+    $mac_gs_vc = array('00:0B:82','AC:A8:8E','00:11:22','C0:74:AD');
 
 ?>
