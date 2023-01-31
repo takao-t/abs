436a1dfc7d55f197af5f849632f59cbf5f75b26e
TakaoTakahashi <tsq@gapj.net>
2023-01-31 16:45:03 +0900

    now support GRP2604P

--- panel/php/addon/gsphone.php
+++ panel/php/addon/gsphone.php
@@ -8,20 +8,26 @@ class GsPhone
   const TITLE = [
     'マスターファイル',
     '端末(ピア)情報',
+    'FA端末(ピア)情報',
     'GXP2130',
-    'GXP2135'];
+    'GXP2135',
+    'GRP2604P'];
 
   const DESC = [
     '各機種共通マスターファイルの生成',
     '各電話機のピア情報と内線情報ファイルの生成',
+    'FA電話機のピア情報と内線情報ファイルの生成',
     'GXP2130用キー情報生成',
-    'GXP2135用キー情報生成'];
+    'GXP2135用キー情報生成',
+    'GRP2604P用キー情報生成'];
 
   const FILE = [
     'addon/prov-gs-master',
     'addon/prov-gs-peer',
+    'addon/prov-gs-fap-peer',
     'addon/prov-gs-gxp2130',
-    'addon/prov-gs-gxp2135'];
+    'addon/prov-gs-gxp2135',
+    'addon/prov-gs-grp2604p'];
 }
 
 ?>
